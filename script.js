ee93cc51cf3676e5a2365916d1b4e9b811365349
anbaximeiza <1029115621ds@gmail.com>
2024-06-20 19:26:01 +1200

    create clipboard reader and error log handler

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function textProcess(rawText){
-    console.log("gagaga")
     var output=rawText.replaceAll(/[\n\r]+/g, " ");
     console.log(output);
+    return output;
 }
 
+function readClipBoard(){
+    try {
+        let rawText= readText();
+        writeToClipboard(textProcess(rawText));
+    } catch (e) {
+      if (e instanceof NotAllowedError){
+        throwError("Unable to access to clipboard, click allow on the top right to access.")
+      } else if(e instanceof NotFoundError){
+        throwError("No text in clipboard.")
+      }
+    } 
+}
 
+function throwError(errorMsg){
+    errorMsg.innerText = `Error: ${text}`;
+}
+
+function writeToClipboard(textString){
+
+}
 
 document.getElementById("from-web").addEventListener("click", function() {
     var rawTextValue = document.getElementById("rawText").value;
     textProcess(rawTextValue);
 });
+
+let errorMsg=document.querySelector("#log")
 
 
 
